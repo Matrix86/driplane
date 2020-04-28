f65330b4c1b4b938419423d776b47fc23b624daf
Gianluca <braga.gianluca@gmail.com>
2020-04-28 17:40:53 +0200

    fix on url filters

--- filters/url.go
+++ filters/url.go
@@ -30,7 +30,7 @@ func NewUrlFilter(p map[string]string) (Filter, error) {
 	}
 	f.cbFilter = f.DoFilter
 
-	f.rUrl = regexp.MustCompile(`(?i)(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?`)
+	f.rUrl = regexp.MustCompile(`(?i)((http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?(([a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5})|([0-9]{1,3}\.){3}[0-9]{1,3})(:[0-9]{1,5})?(\/[^\s]+)?)`)
 
 	if v, ok := f.params["http"]; ok && v == "false" {
 		f.getHttp = false
@@ -55,28 +55,24 @@ func (f *URL) DoFilter(msg *data.Message) (bool, error) {
 	match := f.rUrl.FindAllStringSubmatch(text, -1)
 	if match != nil {
 		for _, m := range match {
-			for i, mm := range m {
-				if i == 0 {
-					continue
-				}
-				if f.getHttp && strings.HasPrefix(strings.ToLower(mm), "http://") {
-					found = true
-				} else if f.getHttps && strings.HasPrefix(strings.ToLower(mm), "https://") {
-					found = true
-				} else if f.getFtp && strings.HasPrefix(strings.ToLower(mm), "ftp://") {
-					found = true
-				}
+			mm := m[0]
+			if f.getHttp && strings.HasPrefix(strings.ToLower(mm), "http://") {
+				found = true
+			} else if f.getHttps && strings.HasPrefix(strings.ToLower(mm), "https://") {
+				found = true
+			} else if f.getFtp && strings.HasPrefix(strings.ToLower(mm), "ftp://") {
+				found = true
+			}
 
-				if f.extractUrl {
-					clone := *msg
-					clone.SetMessage(mm)
-					clone.SetExtra("fulltext", text)
-					f.Propagate(&clone)
-					// We need to stop the propagation of the first message
-					found = false
-				} else if found {
-					break
-				}
+			if f.extractUrl {
+				clone := *msg
+				clone.SetMessage(mm)
+				clone.SetExtra("fulltext", text)
+				f.Propagate(&clone)
+				// We need to stop the propagation of the first message
+				found = false
+			} else if found {
+				break
 			}
 		}
 	}
