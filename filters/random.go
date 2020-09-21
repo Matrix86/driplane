456461b4085f8ec865857ed9b3509842abf7d27a
Gianluca <braga.gianluca@gmail.com>
2020-09-21 17:41:15 +0200

    new random filter added

---
+++ filters/random.go
@@ -0,0 +1,73 @@
+package filters
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+
+	"github.com/Matrix86/driplane/data"
+
+	"github.com/evilsocket/islazy/log"
+)
+
+// Random is a Filter to create a random number
+type Random struct {
+	Base
+
+	output string
+	min   int64
+	max     int64
+
+	params map[string]string
+}
+
+// NewRandomFilter is the registered method to instantiate a RandomFilter
+func NewRandomFilter(p map[string]string) (Filter, error) {
+	f := &Random{
+		params: p,
+		output: "main",
+		min: 0,
+		max: 999999,
+	}
+	f.cbFilter = f.DoFilter
+
+
+	if v, ok := p["output"]; ok {
+		f.output = v
+	}
+	if v, ok := p["min"]; ok {
+		i, err := strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		f.min = i
+	}
+	if v, ok := p["max"]; ok {
+		i, err := strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		f.max = i
+	}
+
+	return f, nil
+}
+
+// DoFilter is the mandatory method used to "filter" the input data.Message
+func (f *Random) DoFilter(msg *data.Message) (bool, error) {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+
+	random := fmt.Sprintf("%d", r1.Int63n(f.max - f.min) + f.min)
+
+	log.Debug("[randomfilter] picked %s", random)
+	msg.SetTarget(f.output, random)
+
+	return true, nil
+}
+
+// Set the name of the filter
+func init() {
+	register("random", NewRandomFilter)
+}
---
+++ src_docs/content/doc/filters/random.md
@@ -0,0 +1,32 @@
+---
+weight: 15
+title: "Random"
+date: 2020-09-16T22:38:02+02:00
+draft: false
+---
+
+## Random
+
+This filter is used to inject an extra field with a random number in the propagated `Message`.  
+
+### Parameters
+
+ | Parameter | Type | Default | Description 
+ | --- | --- | --- | --- |
+ | **output** | _STRING_ | "main" | the field of the propagated `Message` that will contain the random number |
+ | **min** | _STRING_ | "0" | the min value of the extracted number is `min` |
+ | **max** | _STRING_ | "999999" | the max value of the extracted number is `max` |
+
+{{< notice info "Example" >}} 
+`... | random(output="random_field", min="9", max="90") | ...`
+{{< /notice >}}
+
+### Output
+
+The output `Message` will be equal to the input one, but it will also include the new random field.
+
+### Examples
+
+{{< alert theme="warning" >}}
+Soon...
+{{< /alert >}} 
