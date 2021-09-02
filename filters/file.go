4ff848854f1de3eea4454612d2d36f1b1d18cce8
Simone Margaritelli <evilsocket@gmail.com>
2021-09-03 01:27:46 +0200

    new: added file filter for reading files

---
+++ filters/file.go
@@ -0,0 +1,38 @@
+package filters
+
+import (
+	"io/ioutil"
+
+	"github.com/Matrix86/driplane/data"
+)
+
+// File is a filter that interprets the input Message as a file path, reads it and prints it.
+type File struct {
+	Base
+}
+
+// NewFileFilter is the registered method to instantiate a File filter
+func NewFileFilter(p map[string]string) (Filter, error) {
+	f := File{}
+	f.cbFilter = f.DoFilter
+	return &f, nil
+}
+
+// DoFilter is the mandatory method used to "filter" the input data.Message
+func (f *File) DoFilter(msg *data.Message) (bool, error) {
+	path := msg.GetMessage().(string)
+	if data, err := ioutil.ReadFile(path); err != nil {
+		return true, err
+	} else {
+		msg.SetMessage(string(data))
+		return true, nil
+	}
+}
+
+// OnEvent is called when an event occurs
+func (f *File) OnEvent(event *data.Event) {}
+
+// Set the name of the filter
+func init() {
+	register("file", NewFileFilter)
+}
