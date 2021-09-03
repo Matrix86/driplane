c5d42d1eefe3944625124312457f0bfdb71ac687
Simone Margaritelli <evilsocket@gmail.com>
2021-09-03 18:13:29 +0200

    new: added log to file filter in case the message does not contain a file path

--- filters/file.go
+++ filters/file.go
@@ -33,6 +33,8 @@ func (f *File) DoFilter(msg *data.Message) (bool, error) {
 			}
 			msg.SetMessage(string(readData))
 			return true, nil
+		} else {
+			log.Info("%s is not a file", path)
 		}
 	}
 	return false, nil
