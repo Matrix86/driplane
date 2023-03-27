49644a489b93457dd5246514c2dd78a9e227334a
Gianluca <244997+Matrix86@users.noreply.github.com>
2023-03-27 19:10:22 +0200

    fix: twitter moved to APIv2

--- feeders/twitter.go
+++ feeders/twitter.go
@@ -4,39 +4,34 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"strconv"
 	"strings"
+	"time"
 
 	"github.com/Matrix86/driplane/data"
-	"github.com/k0kubun/pp"
-
-	//"github.com/dghubble/go-twitter/twitter"
-	//"github.com/dghubble/oauth1"
-
-	twitter "github.com/g8rswimmer/go-twitter/v2"
 
 	"github.com/evilsocket/islazy/log"
+	twitter "github.com/g8rswimmer/go-twitter/v2"
+	"github.com/k0kubun/pp"
 )
 
 // Twitter is a Feeder that feeds a pipeline with tweets
 type Twitter struct {
 	Base
 
-	consumerKey    string
-	consumerSecret string
-	accessToken    string
-	accessSecret   string
-	bearerToken    string
+	bearerToken string
 
 	keywords      string
 	twitterRule   string
 	users         string
-	languages     string
+	languages     map[string]bool
 	retweet       bool
 	quoted        bool
 	stallWarnings bool
 	closeChan     chan int
 
+	retry int
+
+	rules  []string
 	client *twitter.Client
 }
 
@@ -48,20 +43,11 @@ func NewTwitterFeeder(conf map[string]string) (Feeder, error) {
 		retweet:       true,
 		quoted:        true,
 		closeChan:     make(chan int),
+		rules:         make([]string, 0),
+		languages:     make(map[string]bool),
+		retry:         10,
 	}
 
-	if val, ok := conf["twitter.consumerKey"]; ok {
-		t.consumerKey = val
-	}
-	if val, ok := conf["twitter.consumerSecret"]; ok {
-		t.consumerSecret = val
-	}
-	if val, ok := conf["twitter.accessToken"]; ok {
-		t.accessToken = val
-	}
-	if val, ok := conf["twitter.accessSecret"]; ok {
-		t.accessSecret = val
-	}
 	if val, ok := conf["twitter.bearerToken"]; ok {
 		t.bearerToken = val
 	}
@@ -75,7 +61,10 @@ func NewTwitterFeeder(conf map[string]string) (Feeder, error) {
 		t.users = val
 	}
 	if val, ok := conf["twitter.languages"]; ok {
-		t.languages = val
+		langs := strings.Split(val, ",")
+		for _, k := range langs {
+			t.languages[strings.TrimSpace(k)] = true
+		}
 	}
 	if val, ok := conf["twitter.disable_retweet"]; ok && val == "true" {
 		t.retweet = false
@@ -83,51 +72,9 @@ func NewTwitterFeeder(conf map[string]string) (Feeder, error) {
 	if val, ok := conf["twitter.disable_quoted"]; ok && val == "true" {
 		t.quoted = false
 	}
-	if val, ok := conf["twitter.stallWarnings"]; ok {
-		b, err := strconv.ParseBool(val)
-		if err != nil {
-			log.Error("Value for 'twitter.stallWarnings' is invalid '%s'", val)
-		} else {
-			t.stallWarnings = b
-		}
-	}
 
 	return t, nil
 }
-
-//func (t *Twitter) getIdsFromUsernames(usernames []string) (map[string]int64, error) {
-//	ids := make(map[string]int64)
-//	// This API supports max 100 ids per request and 300 requests/15min window
-//	for start, end := 0, 100; start < len(usernames); start, end = start+100, end+100 {
-//		if end > len(usernames) {
-//			end = len(usernames)
-//		}
-//		params := &twitter.UserLookupParams{ScreenName: usernames[start:end]}
-//		idsList, _, err := t.client.Users.Lookup(params)
-//		if err != nil {
-//			if err.Error() == "twitter: 88 Rate limit exceeded" {
-//				log.Info("Twitter rate limit exceeded...waiting 15 minutes")
-//				time.Sleep(15 * time.Minute)
-//				continue
-//			} else {
-//				return nil, err
-//			}
-//		}
-//
-//		for _, u := range idsList {
-//			ids[u.ScreenName] = u.ID
-//		}
-//	}
-//	return ids, nil
-//}
-
-//func (t *Twitter) getTweetExtendedText(tweet *twitter.Tweet) string {
-//	text := tweet.Text
-//	if tweet.ExtendedTweet != nil {
-//		text = tweet.ExtendedTweet.FullText
-//	}
-//	return text
-//}
 
 type authorize struct {
 	Token string
@@ -138,14 +85,14 @@ func (a authorize) Add(req *http.Request) {
 }
 
 func (t *Twitter) getUsernameFromIds(IDs []string) ([]string, error) {
-	res, err := t.client.UserLookup(context.Background(), IDs, twitter.UserLookupOpts{UserFields: []twitter.UserField{twitter.UserFieldName}})
+	res, err := t.client.UserLookup(context.Background(), IDs, twitter.UserLookupOpts{UserFields: []twitter.UserField{twitter.UserFieldUserName}})
 	if err != nil {
 		return nil, fmt.Errorf("can't retrieve info for userID %s : %s", strings.Join(IDs, ","), err)
 	}
 	if len(res.Raw.Users) != 0 {
 		names := make([]string, len(res.Raw.Users))
 		for idx, n := range res.Raw.Users {
-			names[idx] = n.Name
+			names[idx] = n.UserName
 		}
 		return names, nil
 	}
@@ -164,24 +111,58 @@ func (t *Twitter) handleTweet(tm *twitter.TweetMessage) {
 			return
 		}
 
-		//if tweet.ReferencedTweets != nil && len(tweet.ReferencedTweets) != 0 {
-		//	if tweet.ReferencedTweets[0].Type == "quoted" {
-		//
-		//	}
-		//}
+		// skip if the filter by language is active
+		if len(t.languages) > 0 {
+			if _, ok := t.languages[tweet.Language]; !ok {
+				continue
+			}
+		}
 
-		t.Propagate(data.NewMessageWithExtra(tweet.Text, map[string]interface{}{
-			"link":          fmt.Sprintf("https://twitter.com/%s/statuses/%d", usernames[0], tweet.ID),
+		msg := data.NewMessageWithExtra(tweet.Text, map[string]interface{}{
+			"link":          fmt.Sprintf("https://twitter.com/%s/statuses/%s", usernames[0], tweet.ID),
 			"language":      tweet.Language,
 			"username":      usernames[0],
 			"source_client": tweet.Source,
-			//"quoted":            "false",
-			//"retweet":           "true",
-			//"original_username": retweet.User.ScreenName,
-			//"original_language": retweet.Lang,
-			//"original_status":   txt,
-			//"original_link":     fmt.Sprintf("https://twitter.com/%s/statuses/%d", retweet.User.ScreenName, retweet.ID),
-		}))
+			"quoted":        "false",
+			"retweet":       "false",
+			"response":      "false",
+		})
+
+		isQuote, isRetweet := false, false
+		extra := make(map[string]string)
+
+		if tweet.ReferencedTweets != nil && len(tweet.ReferencedTweets) != 0 {
+			for idx := range tweet.ReferencedTweets {
+				if tweet.ReferencedTweets[idx].Type == "quoted" {
+					extra["quoted"] = "true"
+					extra["original_link"] = fmt.Sprintf("https://twitter.com/dummy/statuses/%s", tweet.ReferencedTweets[idx].ID)
+					isQuote = true
+				} else if tweet.ReferencedTweets[idx].Type == "replied_to" {
+					extra["response"] = "true"
+					extra["original_link"] = fmt.Sprintf("https://twitter.com/dummy/statuses/%s", tweet.ReferencedTweets[idx].ID)
+				} else if tweet.ReferencedTweets[idx].Type == "retweeted" {
+					extra["retweet"] = "true"
+					extra["original_link"] = fmt.Sprintf("https://twitter.com/dummy/statuses/%s", tweet.ReferencedTweets[idx].ID)
+					isRetweet = true
+				}
+			}
+		}
+		if tweet.InReplyToUserID != "" {
+			extra["reply_for_user"] = tweet.InReplyToUserID
+		}
+		for k, v := range extra {
+			msg.SetExtra(k, v)
+		}
+
+		// do not share if retweets and quotes are disabled
+		if !t.retweet && isRetweet {
+			continue
+		}
+		if !t.quoted && isQuote {
+			continue
+		}
+
+		t.Propagate(msg)
 	}
 }
 
@@ -200,23 +181,69 @@ func (t *Twitter) Start() {
 
 	// Setup rules
 	log.Debug("Adding Twitter Rules")
+	rules := []twitter.TweetSearchStreamRule{}
 	if t.keywords != "" {
 		keywords := strings.Split(t.keywords, ",")
 		for i, k := range keywords {
 			keywords[i] = strings.TrimSpace(k)
 		}
-		log.Debug("Setting keywords: '%s'", strings.Join(keywords, ","))
+		log.Debug("Setting keywords rule: '%s'", strings.Join(keywords, ","))
 
-		streamRule := twitter.TweetSearchStreamRule{
+		keywordRule := twitter.TweetSearchStreamRule{
 			Value: strings.Join(keywords, " OR "),
-			Tag:   "keywords",
+			Tag:   "keywords_rule",
 		}
 
-		searchStreamRules, err := t.client.TweetSearchStreamAddRule(context.Background(), []twitter.TweetSearchStreamRule{streamRule}, false)
-		if err != nil {
-			log.Fatal("can't create a rule for Twitter stream: %s", err)
+		t.rules = append(t.rules, strings.Join(keywords, " OR "))
+		rules = append(rules, keywordRule)
+	}
+
+	if t.users != "" {
+		users := strings.Split(t.users, ",")
+		for i, k := range users {
+			users[i] = fmt.Sprintf("@%s", strings.TrimSpace(k))
 		}
-		log.Debug("Rule created: %#v", searchStreamRules)
+		log.Debug("Setting users rule: '%s'", strings.Join(users, ","))
+
+		userRule := twitter.TweetSearchStreamRule{
+			Value: strings.Join(users, " OR "),
+			Tag:   "users_rule",
+		}
+
+		t.rules = append(t.rules, strings.Join(users, " OR "))
+		rules = append(rules, userRule)
+	}
+
+	if t.twitterRule != "" {
+		log.Debug("Setting custom rule: '%s'", t.twitterRule)
+
+		customRule := twitter.TweetSearchStreamRule{
+			Value: t.twitterRule,
+			Tag:   "custom_rule",
+		}
+
+		t.rules = append(t.rules, t.twitterRule)
+		rules = append(rules, customRule)
+	}
+
+	if len(rules) > 0 {
+		log.Debug("TwitterFeeder: adding %d rules", len(t.rules))
+		searchStreamRules, err := t.client.TweetSearchStreamAddRule(context.Background(), rules, false)
+		if err != nil {
+			log.Fatal("TwitterFeeder: can't create a rule for Twitter stream: %s", err)
+		}
+
+		if searchStreamRules.Errors != nil && len(searchStreamRules.Errors) > 0 {
+			for _, e := range searchStreamRules.Errors {
+				log.Error("TwitterFeeder: error adding rule '%s': %s", e.Value, e.Title)
+			}
+		}
+
+		if searchStreamRules.Rules != nil {
+			log.Debug("TwitterFeeder: %d rules have been created", len(searchStreamRules.Rules))
+		}
+	} else {
+		log.Info("TwitterFeeder: no rule found, waiting for rules specified externally")
 	}
 
 	opts := twitter.TweetSearchStreamOpts{
@@ -243,133 +270,47 @@ func (t *Twitter) Start() {
 				return
 
 			case tm := <-tweetStream.Tweets():
-				//log.Info("Tweet received: %s", pp.Sprint(tm))
 				t.handleTweet(tm)
 
 			case sm := <-tweetStream.SystemMessages():
-				log.Error("System message received: %#v", sm)
+				log.Info("TwitterFeeder: System message received: %#v", sm)
 
 			case de := <-tweetStream.DisconnectionError():
-				log.Error("Disconnection error: %#v", pp.Sprint(de))
+				log.Error("TwitterFeeder: disconnection error: %#v", pp.Sprint(de))
 
 			case strErr := <-tweetStream.Err():
-				log.Error("Error: %#v", strErr)
+				log.Error("TwitterFeeder: error on the stream: %#v", strErr)
 
 			default:
 			}
 			if !tweetStream.Connection() {
 				log.Error("Connection retry")
-				t.Start()
+				if t.retry > 0 {
+					t.retry--
+					time.Sleep(10 * time.Second)
+					t.Start()
+				} else {
+					log.Error("Connection retries finished...need to restart the app")
+				}
 				return
+			} else {
+				// resetting the retries
+				t.retry = 10
 			}
 		}
 	}()
-
-	//log.Debug("Setting demuxer")
-	//// Convenience Demux demultiplexed stream messages
-	//demux := twitter.NewSwitchDemux()
-	//demux.Tweet = func(tweet *twitter.Tweet) {
-	//	if t.retweet && tweet.RetweetedStatus != nil {
-	//		if tweet.RetweetedStatus != nil {
-	//			retweet := tweet.RetweetedStatus
-	//			txt := t.getTweetExtendedText(retweet)
-	//			t.Propagate(data.NewMessageWithExtra(txt, map[string]interface{}{
-	//				"link":              fmt.Sprintf("https://twitter.com/%s/statuses/%d", tweet.User.ScreenName, tweet.ID),
-	//				"language":          tweet.Lang,
-	//				"username":          tweet.User.ScreenName,
-	//				"quoted":            "false",
-	//				"retweet":           "true",
-	//				"original_username": retweet.User.ScreenName,
-	//				"original_language": retweet.Lang,
-	//				"original_status":   txt,
-	//				"original_link":     fmt.Sprintf("https://twitter.com/%s/statuses/%d", retweet.User.ScreenName, retweet.ID),
-	//			}))
-	//		}
-	//	} else if t.quoted && tweet.QuotedStatus != nil {
-	//		if tweet.QuotedStatus != nil {
-	//			quoted := tweet.QuotedStatus
-	//			txt := t.getTweetExtendedText(tweet)
-	//			t.Propagate(data.NewMessageWithExtra(txt, map[string]interface{}{
-	//				"link":              fmt.Sprintf("https://twitter.com/%s/statuses/%d", tweet.User.ScreenName, tweet.ID),
-	//				"language":          tweet.Lang,
-	//				"username":          tweet.User.ScreenName,
-	//				"status":            txt,
-	//				"quoted":            "true",
-	//				"retweet":           "false",
-	//				"original_username": quoted.User.ScreenName,
-	//				"original_status":   t.getTweetExtendedText(quoted),
-	//				"original_language": quoted.Lang,
-	//				"original_link":     fmt.Sprintf("https://twitter.com/%s/statuses/%d", quoted.User.ScreenName, quoted.ID),
-	//			}))
-	//		}
-	//	} else {
-	//		text := t.getTweetExtendedText(tweet)
-	//		t.Propagate(data.NewMessageWithExtra(text, map[string]interface{}{
-	//			"link":     fmt.Sprintf("https://twitter.com/%s/statuses/%d", tweet.User.ScreenName, tweet.ID),
-	//			"language": tweet.Lang,
-	//			"username": tweet.User.ScreenName,
-	//			"status":   text,
-	//			"quoted":   "false",
-	//			"retweet":  "false",
-	//		}))
-	//	}
-	//}
-	//
-	//// we don't track Direct Messages or Events
-	//demux.DM = func(dm *twitter.DirectMessage) {
-	//	log.Debug("[DIRECTMESSAGE] %s", dm.Text)
-	//}
-	//demux.Event = func(event *twitter.Event) {
-	//	log.Debug("[EVENT] %s", event.Event)
-	//}
-	//
-	//// FILTER
-	//filterParams := &twitter.StreamFilterParams{
-	//	StallWarnings: twitter.Bool(true),
-	//}
-	//if t.users != "" {
-	//	users := strings.Split(t.users, ",")
-	//	for i, k := range users {
-	//		users[i] = strings.TrimSpace(k)
-	//	}
-	//	ids, err := t.getIdsFromUsernames(users)
-	//	if err != nil {
-	//		log.Error("getIdsFromUsernames: %s", err)
-	//	} else {
-	//		idss := []string{}
-	//		for _, v := range ids {
-	//			idss = append(idss, fmt.Sprintf("%d", v))
-	//		}
-	//		log.Debug("Setting users to follow: '%s' [ids: %s]", strings.Join(users, ","), strings.Join(idss, ","))
-	//		filterParams.Follow = idss
-	//	}
-	//}
-	//if t.keywords != "" {
-	//	keywords := strings.Split(t.keywords, ",")
-	//	for i, k := range keywords {
-	//		keywords[i] = strings.TrimSpace(k)
-	//	}
-	//	log.Debug("Setting keywords: '%s'", strings.Join(keywords, ","))
-	//	filterParams.Track = keywords
-	//}
-	//if t.languages != "" {
-	//	languages := strings.Split(t.languages, ",")
-	//	filterParams.Language = languages
-	//}
-	//t.stream, err = t.client.Streams.Filter(filterParams)
-	//if err != nil {
-	//	log.Fatal("twitter stream filter error: %s", err)
-	//}
-	//
-	//// Receive messages until stopped or stream quits
-	//go demux.HandleChan(t.stream.Messages)
 	t.isRunning = true
-	log.Warning("OLE")
 }
 
 // Stop handles the Feeder shutdown
 func (t *Twitter) Stop() {
-	// TODO: delete rules
+	if len(t.rules) > 0 {
+		log.Debug("TwitterFeeder: removing %d rules", len(t.rules))
+		_, err := t.client.TweetSearchStreamDeleteRuleByValue(context.Background(), t.rules, false)
+		if err != nil {
+			log.Error("twitterFeeder: couldn't delete the rules: %s", err)
+		}
+	}
 
 	log.Debug("feeder '%s' stream stop", t.Name())
 	close(t.closeChan)
